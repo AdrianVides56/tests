40f6cd31840e0fc73a83c85c48ca0d07c6690a56
andresgfranco <andresgfranco0@gmail.com>
2020-11-17 19:57:05 +0000

    Adding requirement of EOF

--- Shell_project/mini_shell/Arguments/exercise_readline.c
+++ Shell_project/mini_shell/Arguments/exercise_readline.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(/*int ac, char *av*/)
+int main(int ac, char *av)
 {
         char *prompt = "hola@shell$ ", *line;
         size_t len = 0;
@@ -9,8 +9,17 @@ int main(/*int ac, char *av*/)
         while (1)
         {
                 printf("%s", prompt);
-                getline(&line, &len, stdin);
-                printf("%s", line);
+		if ((getline(&line, &len, stdin) == EOF))
+		{
+			printf("\n");
+			exit(EXIT_SUCCESS);			       	/** here we use stdin since we 
+										  * are not receiving input from a file but keyboard
+										  * also we don't need the ac and av arguments, 
+										  * because the getline will get the input from 
+										  * the user
+										 **/
+		}
+		printf("%s", line);
         }
 
         return 0;
