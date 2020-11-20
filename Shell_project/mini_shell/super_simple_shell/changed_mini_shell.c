e03aebd90cea61c5201e96a3761fc8ee81038a10
AndresSern <andrecamposer@gmail.com>
2020-11-20 15:25:10 +0000

    Cambios de condiciones en la mini shell

--- Shell_project/mini_shell/super_simple_shell/changed_mini_shell.c
+++ Shell_project/mini_shell/super_simple_shell/changed_mini_shell.c
@@ -6,77 +6,83 @@
 #include <sys/wait.h>
 
 #define DELIM " ,!¡¿?\'\"\n\t"
-#define GREEN_T "\x1b[32m"
-#define RESET_COLOR "\x1b[37m"
-
 char *_strcat(char *dest, char *src);
-int main(void)
+int main(int ac, char **av, char **envp)
 {
-	char *prompt = "hola@shell$ ", *buffer;
-	size_t bufsize = 1024;
-	pid_t child_pid;
-	char *token = NULL, *token2[1024];
-	int reset, i;
-	unsigned int getln;
+        char *prompt = "hola@shell$ ", *BUFFER;
+        size_t bufsize = 1024;
+        pid_t child_pid;
+        char *token = NULL, *token2[1024];
+        int reset, i;
+        unsigned int getln;
 
-	while (1)
-	{
-		i = 0;
-		reset = 0;
-		char path[60] = "/bin/";
-		if (isatty(STDOUT_FILENO) == 1)
-		        printf(GREEN_T "%s" RESET_COLOR, prompt);
+        while (1)
+        {
+                i = 0;
+                reset = 0;
 
-		getln = getline(&buffer, &bufsize, stdin);
-		if (getln == EOF)
-		{
-			printf("\n");
-			exit(EXIT_SUCCESS);
-		}
+                char path[60] = "/bin/";
+                char path2[20] = "/sbin/";
+                if (isatty(STDOUT_FILENO) == 1)
+                        printf("$ ");
+                printf("%s", prompt);
 
-		token = strtok(buffer, DELIM);
-		while (token != NULL)
-       		{
-       			token2[i] = token;
-       			token = strtok(NULL, DELIM);
-       			i++;
-       		}
-		
-		child_pid = fork();
-		if (child_pid == -1)		
-		{
-			perror("Error");
-			return (1);
-		}
-		
+                getln = getline(&BUFFER, &bufsize, stdin);
+                if (getln == EOF)
+                {
+                        printf("exit\n");
+                        printf("\n");
+                        exit(EXIT_SUCCESS);
+                }
 
-		if (child_pid == 0)
-		{
-		  if (execve(_strcat(path, token2[0]), token2, NULL) == -1)
-       			{
-       				perror("Error");
-			}
-			exit(0);
-		}
-		else
-			child_pid = wait(NULL);
-		for (;reset <= i; reset ++)
-			token2[reset] = NULL;
-	}
-	return (0);
+                token = strtok(BUFFER, DELIM);
+                while (token != NULL)
+                {
+                        token2[i] = token;
+                        token = strtok(NULL, DELIM);
+                        i++;
+                }
+                if (token2[0] != NULL)
+                {
+                        if(strcmp(token2[0], "exit") == 0)
+                        {
+                                printf("\n-"); //PRODUCE DOS SALTOS DE LINEA
+                                exit(0);
+                        }
+                }
+                child_pid = fork();
+                if (child_pid == -1)
+                {
+                        perror("Error");
+                        return (1);
+                }
+                if (child_pid == 0)
+                {
+                        execve(_strcat(path, token2[0]), token2, NULL);
+                        execve(_strcat(path2, token2[0]), token2, NULL);
+
+                        exit(0);
+                }
+                else
+                        child_pid = wait(NULL);
+                for (;reset <= i; reset ++)
+                        token2[reset] = NULL;
+        }
+        return (0);
 }
+
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
+        int a, b;
 
-	for (a = 0; dest[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; src[b] != '\0'; b++)
-	{
-	dest[a] = src[b];
-	a++;
-	}
-	dest[a] = '\0';
-	return (dest);
+        for (a = 0; dest[a] != '\0'; a++)
+        {
+        }
+        for (b = 0; src[b] != '\0'; b++)
+        {
+        dest[a] = src[b];
+        a++;
+        }
+        dest[a] = '\0';
+        return (dest);
 }
