8e4855579241a00a5ed57fc94e4db53921e54f0f
Adrian5666 <62396843+AdrianVides56@users.noreply.github.com>
2020-11-22 11:03:14 -0500

    Update functions2.c

--- Shell_project/mini_shell/Executing_a_program/pruebaenv/functions2.c
+++ Shell_project/mini_shell/Executing_a_program/pruebaenv/functions2.c
@@ -2,59 +2,67 @@
 
 char *_getenv(char **myEnvp, char *path)
 {
-	char *envpath;
-	int count = 0;
+        char *envpath;
+        int count = 0;
 
-	while(myEnvp[count] != NULL)
-	{
-		envpath = _strstr(myEnvp[count], path);
-			if (envpath == NULL)
-				count++;
-			else
-			{
-				return (envpath);
-			}
-	}
-	return (NULL);
+        while(myEnvp[count] != NULL)
+        {
+                envpath = _strstr(myEnvp[count], path);
+                        if (envpath == NULL)
+                                count++;
+                        else
+                        {
+                                return (envpath);
+                        }
+        }
+        return (NULL);
 }
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *c;
+        char *c;
 
-	while (haystack)
-	{
-		while (*needle == *haystack)
-			haystack++, needle++;
-		if (*needle == '\0')
-		{
-			haystack++;
-			c = haystack;
-			return (c);
-		}
-		haystack++;
-	}
-	return (NULL);
+        while (haystack)
+        {
+                while (*needle == *haystack)
+                        haystack++, needle++;
+                if (*needle == '\0')
+                {
+                        haystack++;
+                        c = haystack;
+                        return (c);
+                }
+                haystack++;
+        }
+        return (NULL);
 }
 
 int _execve(char *path, char *command/*, const char *ep*/)
 {
-	char *token = NULL, *token2[1024], *execute;
-	int i = 0, check = 0;
+        char *token = NULL, *token2[1024], *execute;
+        int i = 0, check = 0, len = 0;
 
-	token = strtok(path, ":");
-	while (token != NULL)
-	{
-		token2[i] = token;
-		token = strtok(NULL, ":");
-		i++;
-	}
-	for (i = 0; token2[i] != NULL; i++)
-	{
-		execute = _strcat(token2[i], command);
-		check = execve(execute, token2, NULL);
-		printf("{%s}---%d\n", execute, check);
-		if (check != -1)
-			return (1);
-	}
+        token = strtok(path, ":");
+        while (token != NULL)
+        {
+                token2[i] = token;
+                token = strtok(NULL, ":");
+                i++;
+        }
+/*      for (i = 0; token2[i] != NULL; i++)
+        _strcat(token2[i], "/");*/
+        for (i = 0; token2[i] != NULL; i++)
+        {
+                check = 0;
+                for (len = 0; token2[i][len] != '\0'; len++)
+                {
+                        if (token2[i][len++] == '\0' && token2[i][len] != '/')
+                                _strcat(token2[i], "/");
+                }
+                execute = _strcat(token2[i], command);
+                check = execve(execute, token2, NULL);
+                if (check != -1)
+                        return (1);
+                printf("%s\n", execute);
+        }
 }
