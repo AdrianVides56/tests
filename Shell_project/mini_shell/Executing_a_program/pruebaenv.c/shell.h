bc30256b3fb06a5af8ab54fd227560b411c9edf6
AdrianVides56 <adrianvj18@gmail.com>
2020-11-21 23:50:17 -0500

    Added pruebaenv

---
+++ Shell_project/mini_shell/Executing_a_program/pruebaenv.c/shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define DELIM " ,!¡¿?\'\"\n\t"
+#define GREEN_T "\x1b[32m"
+#define RESET_COLOR "\x1b[37m"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+char *_strcat(char *dest, char *src);
+char **_strtok(char *line);
+size_t _strlen(char *str);
+void errors(int errornum);
+int _execve(char *path, char *argv/*, const char *ep*/);
+char *_getenv(char **myEnvp, char *path);
+char *_strstr(char *haystack, char *needle);
+
+#endif
