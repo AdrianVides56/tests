8b09aef5b305a84aa3044dfd7399413e18b70f1a
AndresSern <andrecamposer@gmail.com>
2020-11-25 05:55:45 +0000

    Changing

--- shell_andreses/tabo/modificando3.c
+++ shell_andreses/tabo/modificando3.c
@@ -5,22 +5,16 @@
 #include <unistd.h>
 
 char *_strcat(char *dest, char *src);
+void execution(char **tokeni, char *path);
 int main(int ac, char **av, char **envp)
 {
         char *prompt = "hola@shell$ ", *buffer = NULL;
         size_t bufsize = 1024;
         pid_t child_pid;
-        int reset, i, getln;
-	char *mypath, **tokenized;
-	struct stat st;
-	char *temp = NULL;
-	char *token = NULL;
-	char *combinar;
-	const char s[2] = ":"; 
+        int getln;
+	char *mypath, **tokenized; 
         while (1)
 	{
-                i = 0;
-                reset = 0;
                 if (isatty(STDOUT_FILENO) == 1)
                         _puts(prompt);
                 getln = getline(&buffer, &bufsize, stdin);
@@ -31,43 +25,19 @@ int main(int ac, char **av, char **envp)
                 tokenized = tokenize(buffer);
                 if (tokenized[0] != NULL && (_strcmp(tokenized[0], "exit") == 0) )
                 {
-                                printf("\n"); 
-                                exit(0);
+			printf("\n"); 
+			exit(0);
                 }
 		mypath = _getenv("PATH");
-
                 child_pid = fork();
-
-                if (child_pid == -1)
-                        return 1;
-                if (child_pid == 0)
-                {
-			if (tokenized[0] != NULL)
-			{
-				if (stat(tokenized[0], &st) == 0)
-				{
-					printf(" FOUND\n");	
-					execve(tokenized[0], tokenized, NULL);
-				}
-        				temp  =  malloc(sizeof(char ) * (strlen(mypath) + 1));
-			        	temp = strcpy(temp, mypath);
-		        		token = strtok(temp, s);
-			   	while( token != NULL )
-        			{
-					combinar = malloc(sizeof(char) * (strlen(token) + strlen(tokenized[0]) + 2));
-					combinar = strcpy(combinar, token);
-					combinar = _strcat(combinar, tokenized[0]);
-					printf("%s\n", combinar);
-					execve(combinar, tokenized, NULL);
-			        	token = strtok(NULL, s);
-          			}
-			        free(temp);
-    			}	
-                }
-                else
-                         wait(NULL);
-				(void)i;
-		(void)reset;
+		if (child_pid == -1)
+			return -1;
+		if (child_pid == 0)
+		{
+			execution(tokenized, mypath);
+		}
+		else
+			wait(NULL);
 		(void)ac;
 		(void)av;
 		(void)envp;
@@ -75,7 +45,7 @@ int main(int ac, char **av, char **envp)
         return (0);
 }
 char *_strcat(char *dest, char *src)
-{
+{ 
         int a, b;
 
         for (a = 0; dest[a] != '\0'; a++)
@@ -90,4 +60,32 @@ char *_strcat(char *dest, char *src)
         }
         dest[a] = '\0';
         return (dest);
+}
+void execution(char **tokeni, char *path)
+{	
+	char *token = NULL, *combinar = NULL, *temp = NULL;
+	const char s[2] = ":";
+	struct stat st;
+		if (stat(tokeni[0], &st) == 0)
+		{
+			printf(" FOUND\n");	
+			execve(tokeni[0], tokeni, NULL);
+		}
+		else
+		{
+        		temp  =  malloc(sizeof(char ) * (strlen(path) + 1));
+			temp = strcpy(temp, path);
+			token = strtok(temp, s);
+			while( token != NULL )
+        		{
+				combinar = malloc(sizeof(char) * (strlen(token) + strlen(tokeni[0]) + 2));
+				combinar = strcpy(combinar, token);
+				combinar = _strcat(combinar, tokeni[0]);
+				printf("%s\n", combinar);
+				execve(combinar, tokeni, NULL);
+				token = strtok(NULL, s);
+				free(combinar);
+          		}
+			free(temp);
+		}
 }
