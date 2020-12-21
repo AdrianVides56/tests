263776fbcfc83a2fb7c5962bd357dac88acec011
AndresSern <andrecamposer@gmail.com>
2020-12-21 22:46:51 +0000

    Putting

---
+++ copy_lifo_fifo/filestest/originalcopy.c
@@ -0,0 +1,39 @@
+#include "header.h"
+#define DELIM " \n"
+
+char *second;
+
+int main()
+{
+	FILE *ifp;
+	size_t len = 0;
+	ssize_t read = 0;	
+	unsigned  int line_number = 1;
+	char *aux = NULL;
+	char *token = NULL;
+	ifp = fopen("monty.txt", "r");
+	while ((read = getline(&aux, &len, ifp)) != -1)
+	{
+		if(*aux != '\n')
+		{
+			if(*aux == ' ')
+				token = strtok(aux, DELIM);
+			if (*aux != ' ')
+				token = strtok(aux, DELIM);
+			if (*token == '#')
+			{
+				continue;
+			}
+			if (check_push(token) != 0 && token != NULL)
+				second = strtok(NULL, DELIM);
+			else
+				second = NULL;
+			printf("%s------%s---\n",token, second);
+			checkFunction(token);
+			line_number += 1;
+		}
+	}
+	free(aux);
+	fclose (ifp);
+	return 0;
+}
