afcb54259ed5bc0bf1d27e73f4b5b997204886d7
AndresSern <andrecamposer@gmail.com>
2020-12-21 03:50:17 +0000

    Adding new files and modify

--- lifo_fifo/prueba.c
+++ lifo_fifo/prueba.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define DELIM " \n"
+char *token;
+int check_push(char *s);
 int main()
 {
-	int j =0;
 	FILE *ifp;
-	const char s[2] = " \n";
 	size_t len = 0;
 	ssize_t read = 0;	
 	unsigned  int line_number = 1;
-	char *aux = NULL, *token = NULL;
-	/*const char opcode = ["push", "pall", "pint", "pop", "swap"];**/
+	char *aux = NULL;
 	ifp = fopen("monty.txt", "r");
 	while ((read = getline(&aux, &len, ifp)) != -1)
 	{
 		if(*aux != '\n')
 		{
-			printf("Line[%d] = ", line_number);
 			if(*aux == ' ')
-				token = strtok(aux, s);
+				token = strtok(aux, DELIM);
 			if (*aux != ' ')
-				token = strtok(aux, s);
-			while(token != NULL && strcpm(token, "push") == 0)
+				token = strtok(aux, DELIM);
+			if (*token == '#')
+				continue;
+			while(token != NULL && check_push(token) != 0)
 			{
-				printf( " %s", token);
-				token = strtok(NULL, s);
-				if (j > 0 &&)
-				{
-					j = 0;
-					break;
-				}
-			j++;
+				token = strcat(token, strtok(NULL, DELIM));
 			}
 			line_number += 1;
-			printf("\n");
 		}
 	}
+	free(aux);
 	fclose (ifp);
 	return 0;
+}
+int check_push(char *s)
+{
+	char *accept = "push";
+	int i = 0;
+	if (s == NULL)
+	{
+		return 0;
+	}
+	while(s[i] == accept[i])
+        {
+            i++;
+        }
+        if (i == 5 && s[i] == ' ')
+	{
+		return 1;
+	}
+        return 0;
 }
